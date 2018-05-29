93a2c3a87fc634007f3777bb951b0add419180fc
dan <danbrianlewis@gmail.com>
2018-05-29 14:12:27 -0400

    add contract tests

---
+++ test/Inbox.test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+//*almost all web3 functions are async*
+
+//creates an instance of the web3 object and connects it to the provider.
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  //get a list of all accts
+  accounts = await web3.eth.getAccounts()
+  //use one of those accts to deploy the contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi There!');
+  });
+
+  it('can change the message' , async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
+  });
+});
