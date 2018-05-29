25cbad1edd79d60b41788ab115e8ddbee9bcbb4d
dan <danbrianlewis@gmail.com>
2018-05-29 16:19:18 -0400

    update deploy.js to reflect err handling

--- deploy.js
+++ deploy.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config()
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
@@ -10,15 +11,27 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+
+  console.log('Running Deploy....')
+
+  try {
+    const accounts = await web3.eth.getAccounts();
+    if(!accounts){
+      console.log('No accounts found');
+    }
+  } catch(err) {
+    console.log(err)
+  }
+  
 
   console.log('Attemping to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi There'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: 1000000, from: accounts[0] });
 
   console.log('deployed at:', result.options.address);
 };
 
 deploy();
+
