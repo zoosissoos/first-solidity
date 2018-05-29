22b48f8db625767b042e975cfba1e8a00398eb27
dan <danbrianlewis@gmail.com>
2018-05-29 14:48:37 -0400

    add deploy scripts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -35,7 +35,7 @@ describe('Inbox', () => {
     assert.equal(message, 'Hi There!');
   });
 
-  it('can change the message' , async () => {
+  it('can change the message', async () => {
     await inbox.methods.setMessage('bye').send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
     assert.equal(message, 'bye');
